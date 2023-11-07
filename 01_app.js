d8a6440971431abca29db41557c1883197529a46
dadabhoge7 <dadabhoge7@gmail.com>
2023-11-07 15:51:25 +0530

    update
    update

--- 01_app.js
+++ 01_app.js
@@ -5,8 +5,23 @@ const app = express();
 const port = 3000;
 
 //end point/route
+// http Methods - Get, Post,Put,Patch,delete etc
+// Syntax of End Point - app.AnyMethod('pth',(req,res)=>{})
 app.get('/',(req,res)=>{
     res.send('Welcome to root URL of server get method');
+})
+
+app.post('/',(req,res)=>{
+    res.send('Welcome to post method');
+})
+
+app.get('/home',(req,res)=>{
+    res.send('Welocome to ExpressJS');
+});
+
+// hoe to send data to server
+app.post('/user',(req,res)=>{
+    res.send('Welcome to user details');
 })
 
 app.listen(port,(error)=>{
